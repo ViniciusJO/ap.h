cca4df6460205d101d7c3d791b26a9b10b50aaf6
ViniciusJO <viniciusjosedeoliveira@outlook.com>
2026-04-06 19:48:47 -0300

    variants, completions, --flag=value syntax and stb-style docs

--- test.c
+++ test.c
@@ -1,20 +1,25 @@
-// FLAG(NAME, SHORT_FORM, TYPE, REQUIRED, DEFAULT_VALUE, DESCRIPTION)
-#define FLAGS_LIST                                 \
-  FLAG(contact_list, cl, char *, false, "default", "Contact list name")  \
-  FLAG(local, l, char*, true, "", "Location")                  \
-  FLAG(DDD, , int, false, 1, "Direct distance dialing code")
+// FLAG(NAME, SHORT_FORM, TYPE, REQUIRED, DEFAULT_VALUE, DESCRIPTION, __VARIADIC_VARIANTS__)
+#define FLAGS_LIST(FLAG)                                                                      \
+  FLAG(local, l, char*, true, "", "Location")                                                 \
+  FLAG(contact_list, cl, char *, false, "default", "Contact list name")                       \
+  FLAG(DDD, , int, false, 1, "Direct distance dialing code", 318, 350)                        \
+  FLAG(x, , int, false, 0, "Direct distance dialing code")
 
-// POS_ARG(NAME, REQUIRED, DEFAULT_VALUE, DESCRIPTION)
-#define POS_ARGS_LIST                \
-  POS_ARG(name, true, "", "Contact name")            \
-  POS_ARG(phone, false, "-", "Phone number")         \
-  POS_ARG(type, false, "commercial", "Type of contact") 
+// POS_ARG(NAME, REQUIRED, DEFAULT_VALUE, DESCRIPTION, __VARIADIC_VARIANTS__)
+#define POS_ARGS_LIST(POS_ARG)                                                                \
+  POS_ARG(name, true, "", "Contact name")                                                     \
+  POS_ARG(phone, false, "-", "Phone number")                                                  \
+  POS_ARG(type, false, "commercial", "Type of contact", commercial, personal, organizational) \
+  POS_ARG(subcommand, false, "help", "Subcmd", A,B,C,D) 
 
 #define AP_ALLOW_EXTRAS
 #define AP_IMPLEMENTATIONS
 #include "ap.h"
 
 #include <stdio.h>
+
+#define T(DEFAULT) #DEFAULT
+
 
 Args tests(int argc, char **argv) {
   Args args = ap_parse_args(argc, argv);
@@ -25,11 +30,9 @@ Args tests(int argc, char **argv) {
   printf("name: %s\n", args.pos_args.name);
   printf("phone: %s\n", args.pos_args.phone);
   printf("type: %s\n", args.pos_args.type);
+  printf("subcommand: %s\n", args.pos_args.subcommand);
 #ifdef AP_ALLOW_EXTRAS
   printf("extras: ");
-  // al_foreach(i, &args.pos_args.extras) {
-  //   printf("\t[%ld]: %s\n", i_idx, *i);
-  // };
   al_print(&args.pos_args.extras, "\n");
 #endif//AP_ALLOW_EXTRAS
   printf("\n");
@@ -43,5 +46,6 @@ Args tests(int argc, char **argv) {
 
 int main(int argc, char **argv) {
   tests(argc, argv);
+  // ap_completions(stdout);
   return 0;
 }
